b80c7f5efff3e2348ccf4a00e61501bfe743e26b
natalia <nathalie.chx3@gmail.com>
2021-02-18 20:27:27 +0100

    buid

---
+++ api/recipes-api/webpack.config.js
@@ -0,0 +1,51 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require('path');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "static/js/bundle.js",
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".json"],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader",
+                }, MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'static/images',
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.tsx?$/, use: {
+                    loader: 'ts-loader',
+                }, exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/, use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'es2015']
+                    }
+                }, exclude: /node_modules/,
+            }
+        ],
+
+    },
+    plugins: [new MiniCssExtractPlugin()]
+
+};
