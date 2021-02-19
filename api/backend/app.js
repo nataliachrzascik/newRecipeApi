1ac178ae48adab3caef849273251a12cf85a5422
natalia <nathalie.chx3@gmail.com>
2021-02-19 12:00:32 +0100

    6

--- api/backend/app.js
+++ api/backend/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, '../recipes-api/build')));
-app.use('/static', express.static(path.join(__dirname, '../recipes-api/build')));
+app.use(express.static(path.join(__dirname, '../recipes-api/build')));
 app.use(fileUpload())
 
 app.get('*', (req, res) => {
